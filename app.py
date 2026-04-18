58c98966d910967095c9e63e10dcab3382964c50
siribankapur <112843270+siribankapur@users.noreply.github.com>
2026-04-17 23:15:36 -0400

    Update app.py

--- app.py
+++ app.py
@@ -23,4 +23,8 @@ def run():
 
     return f"<pre>{output}</pre>"
 
-app.run()
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
